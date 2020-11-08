49afad6c2c79024a06e0af298640841afb7b43df
Deni678 <32527466+Deni678@users.noreply.github.com>
2020-11-08 13:04:34 -0500

    Update lesson1_script.R
    updated saving as image

--- lesson1_script.R
+++ lesson1_script.R
@@ -1,17 +1,18 @@
 library(minfi)
 library(dplyr)
+library(GEOquery)
+
+
 data<- readRDS("meth_data_samples.csv")
-
-
-minfi::densityPlot(data)
 # Turning the data into a matrix
-# 
 matrix_data<-as.matrix((data))
 # Density Plot
 density_beta<-minfi::densityPlot(matrix_data)
-save.image("density_beta",density_beta)
-# save density plot 
+## save density plot 
+save.image("density_beta.png",density_beta)
+
 # Bean plot
 # save bean plot
 bean_plot<-minfi::densityBeanPlot(matrix_data)
-save.image("bean_plot_beta",bean_plot)
+save.image("bean_plot_beta.png",bean_plot)
+
