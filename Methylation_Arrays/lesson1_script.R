abb07d28a0b6a6d6fd4cd736de4511788aba6f97
Almas <khnalmas@gmail.com>
2020-11-08 10:17:31 -0800

    updated with png

--- Methylation_Arrays/lesson1_script.R
+++ Methylation_Arrays/lesson1_script.R
@@ -1,18 +1,21 @@
 library(minfi)
 library(dplyr)
 library(GEOquery)
-
+library(here)
 
 data<- readRDS("meth_data_samples.csv")
 # Turning the data into a matrix
 matrix_data<-as.matrix((data))
 # Density Plot
-density_beta<-minfi::densityPlot(matrix_data)
+density_beta <- minfi::densityPlot(matrix_data)
+
 ## save density plot 
-save.image("density_beta.png",density_beta)
+png("density_beta.png",density_beta)
+
 
 # Bean plot
 # save bean plot
-bean_plot<-minfi::densityBeanPlot(matrix_data)
-save.image("bean_plot_beta.png",bean_plot)
+bean_plot <- minfi::densityBeanPlot(matrix_data)
+png("bean_plot_beta.png",bean_plot)
+
 
